f64bd169170c97f998dcc4e28947985b892d17d2
Phil <pamramfam@gmail.com>
2024-03-09 11:00:43 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValues] = useState(''); 
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault(); 
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, { text: inputValue, completed: false }]); 
+      setInputValues(''); 
+    }
+  }; 
+
+  const toggleToDoCompletion = (index) => {
+    const updatedTodos = todos.map((todo, i) => {
+      index === i ? { ...todo, completed: !todo.completed } : todo
+    }); 
+  }
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((todo, i) => index !== i); 
+    setTodos(updatedTodos); 
+  }
+
+  return (
+    <div className="App">
+      <h1>To-Do's</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter a todo"
+          value={inputValue}
+          onChange={(e) => setInputValues(e.target.value)}
+          autoComplete="off"
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul className="todos">
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.completed ? 'completed' : ''}>
+            <span onClick={() => toggleToDoCompletion(index)}>{todo.text}</span>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App
